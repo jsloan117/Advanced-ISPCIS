311a9264cf3bd9d0e7139fecfd7cbe9bbfffc0dd
Jonathan Sloan <jsloan117@gmail.com>
2016-01-30 11:07:18 -0500

    Update python_installer.sh

--- python_installer.sh
+++ python_installer.sh
@@ -2,16 +2,18 @@
 #=================================================================================================
 # Name:                 python_instaler.sh
 # By:                   Jonathan M. Sloan <jsloan@macksarchive.com>
-# Date:                 11-12-2015
+# Date:                 01-29-2016
 # Purpose:              Install multiple versions of python
-# Version:              1.0
-# This Script will Download 3 additional versions of Python. The versions are 2.7.10, 3.4.3, 3.5.0
+# Version:              1.1
+# This Script will Download 3 additional versions of Python. The versions are 2.7.11, 3.4.4, 3.5.1
 #==================================================================================================
+# ChangeLog:
+# 1.0 -> 1.1: updated versions, added get_pip function
 
 status="$?"
-ver='1.0'
+ver='1.1'
 
-declare -a my_python_versions=('2.7.10' '3.4.3' '3.5.0')
+declare -a my_python_versions=('2.7.11' '3.4.4' '3.5.1')
 
 print_usage(){
 clear; cat<<USAGE
@@ -69,6 +71,16 @@ local python_version=$(echo $pythonversion | cut -d. -f1,2)
 ln -s /usr/local/bin/python$python_version /usr/bin/python$python_version
 }
 
+get_pip () {
+local pip_url='https://bootstrap.pypa.io/get-pip.py'
+
+curl -S -O $pip_url
+
+/usr/bin/python$python_version get-pip.py
+
+rm -f get-pip.py
+}
+
 if [[ $# -ne 0 || $1 = '--help' ]]; then
 
     print_usage
@@ -78,13 +90,20 @@ else
     if [[ $status -eq 0 ]]; then
 
         clear; echo ""; read -ep "What python version do you want to install? ($(echo ${my_python_versions[@]} | sed "s/ /|/g")): " pythonversion ; echo ""
+        read -ep "Do you want to instal pip as well? " yesno
 
-        yum install -y -q zlib-dev openssl-devel sqlite-devel bzip2-devel > /dev/null 2>&1
+        yum install -y -q zlib-dev openssl-devel sqlite-devel bzip2-devel
         make_dirs
         download_python_versions
         extract_tars
         compile_python_versions
         make_symlinks
+
+        if [[ $yesno = 'yes' ]]; then
+
+          get_pip
+
+        fi
 
     fi
 
